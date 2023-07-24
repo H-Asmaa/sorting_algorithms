0f0c581ed45e3b45a4f2a0828ba6a6fa25ec3ca7
H-Asmaa <asmaehadar32@gmail.com>
2023-07-24 06:51:49 +0100

    Committing bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort the array
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, tmp;
+
+	j = 0;
+	i = size;
+
+	while (i > 0)
+	{
+		j = 0;
+		while (j + 1 < i)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
+			}
+			print_array(array, size);
+			j++;
+		}
+		i--;
+	}
+}
