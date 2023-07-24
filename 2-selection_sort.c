ac4968732b153875a363ed4a3213dcc0dfad3dd9
OUALIID <walidelhadim8@gmail.com>
2023-07-24 10:01:55 +0100

    add complexity and comment

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,13 +1,20 @@
 #include "sort.h"
 
+/**
+ * selection_sort - sort the array
+ *
+ * @array: The array to be printed
+ * @len: len of elements in @array
+ */
 void selection_sort(int *array, int len)
 {
 	int tmp, index;
-    int i, j;
-	for ( i = 0; i < len; i++)
+	int i, j;
+
+	for (i = 0; i < len; i++)
 	{
 		index = i;
-		for ( j = i + 1; j < len; j++)
+		for (j = i + 1; j < len; j++)
 		{
 			if (array[index] > array[j])
 				index = j;
